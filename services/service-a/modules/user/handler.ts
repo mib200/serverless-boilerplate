60cc30af686ded428f9767c0105b20e0f78c080b
Manish Kumar <mib200@gmail.com>
2020-12-04 23:33:34 +0530

    sample payload model validation

--- services/service-a/modules/user/handler.ts
+++ services/service-a/modules/user/handler.ts
@@ -1,3 +1,5 @@
+import userSchema from '../models/user-schema.json';
+
 module.exports = (config) => {
   return {
     'user-GET': {
@@ -17,7 +19,26 @@ module.exports = (config) => {
         {
           http: {
             method: 'put',
-            path: `${config.custom.urlBasePath}user`,
+            path: `${config.custom.urlBasePath}user/{id}`,
+            request: {
+              schema: {
+                'application/json': userSchema,
+              },
+              parameters: {
+                // querystrings: { url: true },
+                // headers: { foo: false },
+                paths: { id: true },
+              },
+              // passThrough: 'WHEN_NO_TEMPLATES',
+              // contentHandling: 'CONVERT_TO_TEXT',
+            },
+            // documentation: {
+            //   summary: 'Create something',
+            //   description: 'Creates a user and then sends a generated password email',
+            //   requestBody: {
+            //     description: 'A user information object',
+            //   },
+            // },
           },
         },
       ],
